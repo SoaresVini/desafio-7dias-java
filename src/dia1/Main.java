7b16f8ad95e728284fdc138cc3482c812af1e8cd
Vini <vini.soares.filho@gmail.com>
2022-10-07 22:45:02 -0300

    dia1

---
+++ src/dia1/Main.java
@@ -0,0 +1,25 @@
+//10. Faça um método que recebe dois números reais ℝ como 
+//parâmetro e retorna a soma de ambos.
+package dia1;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	
+	System.out.print("Número 1:");
+	Integer num1 = Integer.valueOf(s.nextLine());
+	
+	System.out.print("Número 2:");
+	Integer num2 = Integer.valueOf(s.nextLine());
+	
+	System.out.println("Soma dos números lidos: " + calcSoma(num1, num2));
+	}
+
+	private static int calcSoma(Integer num1, Integer num2) {
+
+		return num1 + num2;
+	}
+
+}
